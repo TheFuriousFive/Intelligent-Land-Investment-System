0acfff6bbc8984e6f3cd158690fb0025576c246c
Selith <selithrubasingha@gmail.com>
2026-02-22 23:10:17 +0530

    initial commit

---
+++ landapp/src/main/java/com/example/landapp/LandappApplication.java
@@ -0,0 +1,13 @@
+package com.example.landapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandappApplication.class, args);
+	}
+
+}
---
+++ landapp/src/test/java/com/example/landapp/LandappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.landapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
