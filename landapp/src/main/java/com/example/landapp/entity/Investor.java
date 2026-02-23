6b9ea3ae361c3fc5842cbe50cfbf9b12f790ccb4
Selith Rubasingha <selithrubasingha@gmail.com>
2026-02-23 17:20:25 +0530

    Investor object created

--- landapp/src/main/java/com/example/landapp/entity/Investor.java
+++ landapp/src/main/java/com/example/landapp/entity/Investor.java
@@ -1,4 +1,18 @@
 package com.example.landapp.entity;
 
-public class Investor {
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "investors")
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Investor extends BaseUser {
+
+    // Inherits id, firstName, and lastName from BaseUser!
+    private String email;
+    private String nationality;
+
+
 }
