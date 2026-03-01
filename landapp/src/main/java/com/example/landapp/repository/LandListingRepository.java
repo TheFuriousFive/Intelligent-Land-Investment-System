fc6feee957278b80529aafee84b5fa4a856437da
Selith <selithrubasingha@gmail.com>
2026-03-01 14:27:32 +0530

    added land listing repository interface

---
+++ landapp/src/main/java/com/example/landapp/repository/LandListingRepository.java
@@ -0,0 +1,12 @@
+package com.example.landapp.repository;
+
+import com.example.landapp.entity.LandListing;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface LandListingRepository extends JpaRepository<LandListing, Long> {
+
+
+}
--- landapp/src/main/java/com/example/landapp/repository/LandRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.landapp.repository;
-
-public class LandRepository {
-}
