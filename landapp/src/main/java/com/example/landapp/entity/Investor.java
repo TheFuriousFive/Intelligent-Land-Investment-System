8e48da93a5fdc8e88523df86e36ac153829da7d4
Nethmika4881 <nethmikakumarasiri@gmail.com>
2026-03-06 11:26:25 +0530

    add

--- landapp/src/main/java/com/example/landapp/entity/Investor.java
+++ landapp/src/main/java/com/example/landapp/entity/Investor.java
@@ -8,6 +8,10 @@ import lombok.EqualsAndHashCode;
 @Table(name = "investors")
 @Data
 @EqualsAndHashCode(callSuper = true)
+<<<<<<< HEAD
+=======
+@NoArgsConstructor
+>>>>>>> bec3dae (backend error)
 public class Investor extends BaseUser {
 
     // Inherits id, firstName, and lastName from BaseUser!
