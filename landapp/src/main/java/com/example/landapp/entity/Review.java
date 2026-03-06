c25967daafd4d06ca99df2acc806d93e6cc6286d
jprashmi413-ops <jprashmi413@gmail.com>
2026-03-06 15:56:09 +0530

    review class and answer class created

---
+++ landapp/src/main/java/com/example/landapp/entity/Review.java
@@ -0,0 +1,31 @@
+package com.example.landapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor //generates a constructor that required one argument for every field
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    @CreationTimestamp
+    @Column(updatable = false,nullable = false)
+    private LocalDateTime createdAt;
+}
