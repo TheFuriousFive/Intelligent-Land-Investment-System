d756528189a7d620289034301086c64e2c1f9738
Selith Rubasingha <selithrubasingha@gmail.com>
2026-03-06 19:01:17 +0530

    ownerservice update

--- landapp/src/main/java/com/example/landapp/service/OwnerService.java
+++ landapp/src/main/java/com/example/landapp/service/OwnerService.java
@@ -104,9 +104,11 @@ public class OwnerService {
         List<LandListing> listings = landRepository.findByOwnerId(ownerId);
 
         // 2. Convert them to Response DTOs and return the list
-        return listings.stream()
-                .map(landMapper::toResponseDTO)
-                .toList();
+
+
+
+
+
     }
 
     public OwnerResponseDTO getOwnerById(Long ownerId) {
